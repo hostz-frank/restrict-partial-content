ebbcb62b22ab67068289d449dff0b18469a50d67
Frank Stuerzebecher <frank@hostz.at>
2014-11-07 13:53:19 +0100

    Security fix for message output. Bugfix: the WP_User object may have several roles and role[0] is not always given.

--- restrict-partial-content.php
+++ restrict-partial-content.php
@@ -3,7 +3,7 @@
  * Plugin Name: Restrict Partial Content
  * Plugin URI: http://wordpress.org/plugins/restrict-partial-content/
  * Description: This plugin helps to protect specific portion of the content
- * Version: 1.1
+ * Version: 1.1.1
  * Author: Waqas Ahmed
  * Author URI: http://speedsoftsol.com
  * License: GPL2
@@ -35,29 +35,32 @@ function render_restrict( $atts, $content = null ) {
 			$restrict = 0;
 		}
 	}
-	
-	
+
+
 	// Find current user role and ID
 	$user_info = wp_get_current_user();
-	$user_role = $user_info->roles[0];
 	$user_id = $user_info->ID;
 
 	$user_list = explode (",", $allow_user);
 	$user_list_trimmed = array_map('trim', $user_list);
-	if ($user_id != NULL && in_array($user_id, $user_list_trimmed)) {
+	if ( 0 !== $user_id && in_array($user_id, $user_list_trimmed) ) {
 		$restrict = 0;
 	}
 
 	$allow_role = strtolower ($allow_role);
 	$role_list = explode (",", $allow_role);
-	$role_list_trimmed = array_map('trim', $role_list);	
-	if ($user_id != NULL && in_array ($user_role, $role_list_trimmed)) {
-		$restrict = 0;
+	$role_list_trimmed = array_map('trim', $role_list);
+	if ( 0 !== $user_id ) {
+		foreach ( $user_info->roles as $user_role ) {
+			if ( in_array ($user_role, $role_list_trimmed) ) {
+				$restrict = 0;
+			}
+		}
 	}
-	
+
 	if ($restrict == 1) {
 		wp_enqueue_style( 'restrict-content', plugins_url().'/restrict-partial-content/restrict-partial.css');
-		$output = '<div class="restricted-content">'.$message.'</div>';
+		$output = '<div class="restricted-content">' .  esc_html ( $message ) . '</div>';
 		if ($interval >0) {
 			$output .= '<div id="timer">
 			<div id="timer-days"></div><div id="timer-hours"></div><div id="timer-minutes"></div><div id="timer-seconds"></div>
@@ -69,21 +72,21 @@ function render_restrict( $atts, $content = null ) {
 	function counter(total_time) {
 		var d = Math.floor(total_time/86400);
 		var remaining_time = total_time - (d*86400);
-		
+
 		var h = Math.floor(remaining_time/3600);
 		var remaining_time = remaining_time - (h*3600);
 
 		var m = Math.floor(remaining_time/60);
 		var remaining_time = remaining_time - (m*60);
 		var s = remaining_time;
-		
+
 		document.getElementById('timer-days').innerHTML = d + ' days';
 		document.getElementById('timer-hours').innerHTML = h + ' hours';
 		document.getElementById('timer-minutes').innerHTML = m + ' minutes';
 		document.getElementById('timer-seconds').innerHTML = s + ' seconds';
-		
+
 		total_time--;
-		if (total_time <0 ) { 
+		if (total_time <0 ) {
 			document.getElementById('timer-message').innerHTML = 'Refresh the page to view the content';
 			document.getElementById('timer').style.display='none';
 			document.getElementById('timer-message').style.display='inline-block';
@@ -92,7 +95,7 @@ function render_restrict( $atts, $content = null ) {
 		setTimeout(function () {counter (total_time)}, 1000);
 
 	}
-	
+
 	counter(".$interval.");
 	</script>";
 		}
